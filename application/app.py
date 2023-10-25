21712afdda3098722659d8b4f461fd1e1df47089
Johan Siegers <j-siegers@outlook.com>
2023-10-25 20:35:55 +0200

    Full funktionalitet utan design.

--- application/app.py
+++ application/app.py
@@ -2,7 +2,8 @@ import urllib.request
 import ssl
 from flask import Flask, render_template, request
 import json
-
+import pandas as pd
+import datetime
 
 app = Flask(__name__)
 
@@ -19,8 +20,8 @@ def index():
 @app.route('/api', methods=['POST'])
 def api_post():
     """
-    Funktion som tar emot inmatad data från formuläret på indexsidan,
-    skickar den till externt API och returnerar data.
+    Funktion som tar emot inmatad data från formuläret på indexsidan
+    och skickar vidare URL:en till funktionen json_to_html.
 
     :return: Sidan result.html med priser för sökt dag
     """
@@ -29,26 +30,106 @@ def api_post():
     month = request.form['month']
     day = request.form['day']
     price_class = request.form['pclass']
-    # API:ets URL med valda datum infogade som f-sträng
-    api_url = f'https://www.elprisetjustnu.se/api/v1/prices/{year}/{month}-{day}_{price_class}'
-    context = ssl._create_unverified_context()
-    json_data = urllib.request.urlopen(api_url, context=context).read()
-    result_data = json.loads(json_data)
-    return render_template('result.html')
+
+    # Dagens datum sparas för jämförelse.
+    todays_date = datetime.datetime.now()
+
+    # Felhantering av inmatade datum.
+    # Datumet görs om till en integer (date) som sedan jämförs mot datumet 2022-11-01
+    # i int format. Vid fel skrivs meddelande ut.
+
+    try:  # Felhanterare om användaren inte fyller i alla fält.
+        date = int(year + month + day)
+    except ValueError:
+        err_msg = 'Vänligen fyll i all fält!'
+        return render_template('index.html', err_msg=err_msg)
+
+    if date < 20221101:
+        err_msg = 'Fel: välj ett datum från 2022-11-01 och framåt!'
+        return render_template('index.html', err_msg=err_msg)
+
+    # Om datumet är längre fram än 1 dag från dagens datum skrivs felmeddelande ut.
+    elif date > 1 + int(todays_date.strftime("%Y%m%d")):
+        err_msg = 'Fel: Du kan max se en dag framåt!'
+        return render_template('index.html', err_msg=err_msg)
+
+    # API:ets URL med valda datum infogade som f-sträng.
+    api_url = f'https://www.elprisetjustnu.se/api/v1/prices/{year}/{month}-{day}_{price_class}.json'
+
+    # URL:en skickas till json_to_html funktionen där den omvandlas till en HTML tabell.
+    html_data = json_to_html(api_url)
+
+    # HTML tabellen skickas till result.html där den skrivs ut.
+    return render_template('result.html', html_data=html_data)
 
 
 @app.route('/result')
 def result():
     """
     Funktion som hanterar utskrift av sökresultat från första
-    sidans formulär
+    sidans formulär.
     """
     return render_template('result.html')
+
+
+@app.errorhandler(404)
+def error_not_found(error):
+    """
+    Felhanterare om användare försöker ansluta till en sida som inte existerar.
+    :return: Felmeddelande
+    """
+    err_msg = 'Sidan du sökte finns inte'
+    return render_template('index.html', err_msg=err_msg)
 
 
 @app.errorhandler(405)
 def method_not_allowed(error):
     """
     Felhanterare om användare försöker ansluta till /api direkt.
+    :return: index.html
     """
     return render_template('index.html')
+
+
+# Ytterligare funktioner utöver Flask.
+
+def json_to_html(url):
+    """
+    Tar emot en URL och skickar sedan en förfrågan till API. Omvandlar svaret
+    från JSON till en lista med dictionarys som formateras i Pandas.
+    :return: HTML tabell
+    """
+    context = ssl._create_unverified_context()
+
+    # Felhantering om API returnerar felaktig data eller inte svarar.
+    try:
+        # Data hämtas från URL:en.
+        json_data = urllib.request.urlopen(url, context=context).read()
+        # Gör om från JSON format till en lista med dictionarys.
+        result_data = json.loads(json_data)
+
+        # Loop för att ändra time_start i varje dictionary till bara timmar och minuter.
+        # Strängen delas vid bokstaven T och timmar och minuter sparas i ny
+        # variabel som sedan uppdaterar ursprungliga time_start.
+        for row in result_data:
+            time_start = row["time_start"].split("T")[1][:5]
+            row["time_start"] = time_start
+
+        # En Pandas DataFrame skapas med innehållet från result_data.
+        df = pd.DataFrame(result_data)
+
+        # Namnen på kolumnerna byts ut så det ser snyggare ut.
+        new_column_names = {'SEK_per_kWh': 'SEK / kWh', 'time_start': 'Tid'}
+        df = df.rename(columns=new_column_names)
+
+        # Utvalda kolumner väljs ut för att skickas till tabellen.
+        columns = ['Tid', 'SEK / kWh']
+        # DataFrame omvandlas till en HTML tabell
+        table_data = df.to_html(columns=columns, classes="table p-5", justify="left")
+
+    # Vid fel returneras ett felmeddelande och index laddas.
+    except Exception as error:
+        err_msg = f'{error} (sidan du sökte finns inte)'
+        return render_template('index.html', err_msg=err_msg)
+
+    return table_data
--- tests/test_1.py
+++ tests/test_1.py
@@ -1,14 +1,21 @@
-import pytest
 import urllib.request
 import ssl
+import sys
+import os
+
+# Används för att korrekt kunna importera application/app i PyCharm
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+from application import app
+
 
 '''
 # Testcases
-1. Testa att servern är igång
-2. Testa att alla endpoints finns
-3. Testa API är nåbar och ger ett svar
-4. Testa att formuläret returnerar förväntad data från result
-5. Testa att servern ger meddelande vid felaktig sida
+1. Testa att servern är igång (index)
+2. Testa att alla endpoints finns (/api och /result)
+3. Testa att API är nåbar och ger ett svar
+4. Testa att json_to_html funktion returnerar förväntad data
+5. Testa att servern ger meddelande vid felaktig sida(404)
 6. Testa att server ger felmeddelande vid felaktigt datum 
 
 '''
@@ -39,8 +46,38 @@ def test_server_running_result():
 
 def test_api_response():
     """
-       Testar att API:et ger ett svar tillbaka
+       Testar att API:et är online och ger ett svar tillbaka
     """
     assert urllib.request.urlopen("https://www.elprisetjustnu.se/api/v1/prices/2023/10-24_SE3.json",
                                   context=context, timeout=10)
 
+
+def test_json_to_html():
+    """
+    Testar funktionen json_to_url att den tar emot en url, öppnar den och skapar en Pandas dataframe.
+    :return: En HTML tabell
+    """
+    url = "https://www.elprisetjustnu.se/api/v1/prices/2023/10-24_SE3.json"
+    response = app.json_to_html(url)
+    assert "</table>" in response
+
+
+def test_page_not_found():
+    """
+    Kontrollerar att servern skriver ut ett felmeddelande när en sida inte finns.
+    """
+    with urllib.request.urlopen("http://127.0.0.1:5000/info", context=context, timeout=10) as response:
+        html = str(response.read())
+    assert "finns inte" in html
+
+# todo:
+'''
+def test_date_error():
+    """
+    Testar att funktionen json_to_url ger ett felmeddelande om datumet är felaktigt.
+    """
+    url = "https://www.elprisetjustnu.se/api/v1/prices/2023/10-27_SE3.json"
+    with app.app_context():
+        response = app.json_to_html(url)
+    assert RuntimeError in response
+'''
